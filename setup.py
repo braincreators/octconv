32e740311e464a8d86f2869c7a10fbfce135ed2e
Marcel Vingerling <marcel.vingerling@braincreators.com>
2020-01-08 15:33:34 +0100

    Add travis deploy

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup
+from os import path
+import os
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 
 requirements = [
@@ -7,12 +14,14 @@ requirements = [
 
 setup(
     name='octconv',
-    version='0.1.0',
+    version=os.getenv('TRAVIS_TAG', '0.1.0'),
     packages=['octconv'],
     url='https://github.com/braincreators/octconv',
     license='MIT',
     install_requires=requirements,
     author='BrainCreators',
     author_email='miguel@braincreators.com',
-    description='Octave Convolution Implementation in PyTorch'
+    description='Octave Convolution Implementation in PyTorch',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
